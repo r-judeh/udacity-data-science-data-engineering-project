489e79a308648329c07736684020525239d2c29e
Raja Judeh <judeh.raja@outlook.com>
2022-03-09 16:45:34 +0300

    add setup.py to create a python package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='disaster_response',
+      version='1.0',
+      description='Udacity Disaster Response Project for the Data Scientist Nanodegree',
+      author='Raja Judeh',
+      author_email='raja.judeh@vonreventlow.com',
+      packages=find_packages(),
+)
